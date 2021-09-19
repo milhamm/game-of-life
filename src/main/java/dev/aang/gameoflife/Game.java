d1732c87933e82c93c385a408f544e6b55eb9c76
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2021-09-19 13:11:24 +0700

    Refactor renaming method getCurrentCell and setCurrentCell, add method addLivingCell.

--- src/main/java/dev/aang/gameoflife/Game.java
+++ src/main/java/dev/aang/gameoflife/Game.java
@@ -2,11 +2,17 @@ package dev.aang.gameoflife;
 
 public class Game {
 
-    Grid grid;
+    Grid grid = new Grid();
 
     public void start(){
-        grid = new Grid();
-        grid.setGridSize(20).displayGrid();
+        grid.setGridSize(20)
+                .initializeGrid()
+                .addLivingCell(2, 1)
+                .addLivingCell(2, 2)
+                .addLivingCell(2, 3)
+                .addLivingCell(1, 3)
+                .addLivingCell(0, 2)
+                .displayGrid();
 
     }
 
--- src/main/java/dev/aang/gameoflife/Grid.java
+++ src/main/java/dev/aang/gameoflife/Grid.java
@@ -16,27 +16,32 @@ public class Grid {
     public Grid initializeGrid(){
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                Cell valueToBeInserted = getCurrentCell(i, j) != Cell.LIVING_CELL ? Cell.DEAD_CELL : Cell.LIVING_CELL;
-                setCurrentCell(valueToBeInserted, i, j);
+                Cell valueToBeInserted = getCell(i, j) != Cell.LIVING_CELL ? Cell.DEAD_CELL : Cell.LIVING_CELL;
+                setCell(valueToBeInserted, i, j);
             }
         }
+        return this;
+    }
+
+    public Grid addLivingCell(int x, int y){
+        setCell(Cell.LIVING_CELL, x, y);
         return this;
     }
 
     public void displayGrid(){
         for (int i = 0; i < this.grid.length; i++) {
             for (int j = 0; j < this.grid[i].length; j++) {
-                System.out.printf("%s ", getCurrentCell(i, j));
+                System.out.printf("%s ", getCell(i, j));
             }
             System.out.println();
         }
     }
 
-    public Cell getCurrentCell(int x, int y){
+    public Cell getCell(int x, int y){
         return this.grid[x][y];
     }
 
-    public void setCurrentCell(Cell value, int x, int y){
+    public void setCell(Cell value, int x, int y){
         this.grid[x][y] = value;
     }
 
